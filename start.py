2acb0905d89de1c2e0fa830aee7dd98550862073
ambidere <xanthiandere@gmail.com>
2017-07-20 04:12:12 +0800

    refactoring to reduce memory consumption (1/2)

--- start.py
+++ start.py
@@ -13,28 +13,41 @@ opt.add_option('-r', '--recursive', dest='recursive', help='Recursive search of 
 
 (options, args) = opt.parse_args()
 
-def get_files_from_folder(folder, recursive):
-    files = []
+def process_folder(folder, recursive):
+    no_faces = []
+    number_of_files = 0
+    number_of_images = 0
+    number_of_images_processed = 0
+    number_of_images_with_faces = 0
+    number_of_images_without_faces = 0
+
     if exists(dirname(folder)):
+        print 'Starting facial recognition check on folder %s' % (folder)
         for (dirpath, dirnames, filenames) in walk(folder):
-            files.extend([fname for fname in filenames if imghdr.what(join(dirpath, fname)) in file_types])
+            print 'Checking files on folder %s' % (dirpath)
+            # files.extend([fname for fname in filenames if imghdr.what(join(dirpath, fname)) in file_types])
+            for file_name in filenames:
+                number_of_files = number_of_files + 1
+                if imghdr.what(join(dirpath, file_name)) in file_types:
+                    number_of_images = number_of_images + 1
+                    print 'Reading image %s' % (file_name)
+                    file_path = join(dirpath, file_name)
+                    image = face_recognition.load_image_file(file_path)
+                    face_locations = face_recognition.face_locations(image)
+                    if len(face_locations) < 1:
+                        print 'Image %s contains no faces' % (file_name)
+                        number_of_images_without_faces = number_of_images_without_faces + 1
+                        no_faces.append((file_name, file_path))
+                    else:
+                        print 'Image %s contains at least one face' % (file_name)
+                        number_of_images_with_faces = number_of_images_with_faces + 1
+                else:
+                    print 'Skipping file %s for facial recognition processing' % (file_name)
+
             if not recursive:
                 break
     else:
         print "FOLDER DOESN'T EXIST"
-    
-    no_faces = []
-    len_files = len(files)
-    index = 1
-    print "%d images to process. Facial recognition starting." % (len_files)
-    for file_name in files:
-        print '[%d/%d] Reading %s' % (index, len_files, file_name)
-        file_path = join(folder, file_name)
-        image = face_recognition.load_image_file(file_path)
-        face_locations = face_recognition.face_locations(image)
-        if len(face_locations) < 1:
-            no_faces.append((file_name, file_path))
-        index = index + 1
     
     if len(no_faces) > 0:
         dumps_folder = join(folder, 'no_face_dumps')
@@ -44,14 +57,18 @@ def get_files_from_folder(folder, recursive):
         for file_name, file_path in no_faces:
             shutil.move(file_path, join(dumps_folder, file_name))
 
-    return files
-    
+    print 'Done processing folder %s' % (folder)
+    print 'Number of files: %s' % (number_of_files)
+    print 'Number of images: %s' % (number_of_images)
+    print 'Number of images processed: %s' % (number_of_images_processed)
+    print 'Number of images with faces: %s' % (number_of_images_with_faces)
+    print 'Number of images without faces: %s' % (number_of_images_without_faces)
 
 if hasattr(options, 'folder') and options.folder is not None:
     files = []
     if hasattr(options, 'recursive') and options.recursive is not None:
-        get_files_from_folder(options.folder, True)
+        process_folder(options.folder, True)
     else:
-        get_files_from_folder(options.folder, False)
+        process_folder(options.folder, False)
 else:
     print "NO FOLDER SUPPLIED"
